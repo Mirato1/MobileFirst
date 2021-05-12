4b66c05815f915922fe45f06b95ca9f3604f859c
Lucas Giménez <luquetti.5@hotmail.com>
2021-05-12 18:23:13 -0300

    Página terminada.
    Página terminada con html css js.
    Responsive.
    Agregué botón para ir arriba.
    Las tablas con JS en responsive.

--- programa.js
+++ programa.js
@@ -1,3 +1,4 @@
+// Meses
 let meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 
 let diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
@@ -10,17 +11,48 @@ for (var i = 0; i < fecha.length; i++) {
   fecha[i].innerHTML = (diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
 }
 
-const arrow_right = document.getElementById("arrow-right")
-const arrow_left = document.getElementById("arrow-left")
-const table_1 = document.getElementById("table-1")
-const table_2 = document.getElementById("table-2")
-arrow_right.onclick = function(){
-   table_1.style.display = "none"
-   table_2.style.display = "grid"
-   arrow_left.style.display = "block"
-}
-arrow_left.onclick = function(){
-    table_1.style.display = "grid"
-    table_2.style.display = "none"
-    arrow_right.style.display = "block"
-}
+// Tablas con sus flechas
+
+const nextBtn = document.getElementById('arrow-right');
+const prevBtn = document.getElementById('arrow-left');
+const card = document.querySelectorAll('.main__currency-table');
+
+nextBtn.addEventListener('click', function() {
+  card.forEach(function(item) {
+    item.classList.toggle('active');
+  })
+})
+
+prevBtn.addEventListener('click', function() {
+  card.forEach(function(item) {
+    item.classList.toggle('active');
+  })
+})
+
+// Scroll Top Botton
+
+const d = document,
+  w = window;
+
+const $scrollBtn = d.querySelector(".scroll-top-btn");
+
+w.addEventListener("scroll", (e) => {
+  let scrollTop = w.pageYOffset || d.documentElement.scrollTop;
+
+  if(scrollTop > 800){
+    $scrollBtn.classList.remove("hidden");
+  }
+  else{
+    $scrollBtn.classList.add("hidden");
+  }
+});
+
+d.addEventListener("click", (e) => {
+  if(e.target.matches(".scroll-top-btn")){
+    w.scrollTo({
+      behavior: "smooth",
+      top: 0
+    });
+  }
+});
+
